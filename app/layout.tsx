dacc099231acfae2dc43e58621b59e1c0569549f
Luann Rodrigues <qvxs9ydh5k@privaterelay.appleid.com>
2025-11-29 21:48:54 -0300

    track

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default async function Layout({
         {/* Google Tag Manager */}
         <Script
           id="gtm-head"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
