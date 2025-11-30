a483a559eb2786de009a34000e3458929bc89da2
Luann Rodrigues <qvxs9ydh5k@privaterelay.appleid.com>
2025-11-29 21:45:03 -0300

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,11 +45,34 @@ export default async function Layout({
   return (
     <html lang="es">
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N534J7MN');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         {isProduction && (
           <HeaderScript content={content} />
         )}
       </head>
       <body className={bodyClassName} suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N534J7MN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {userLayer === 1 ?
           <WhiteContent />
         : (
