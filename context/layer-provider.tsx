238c39c2e870db362d03e26f2edc9e565f3d3467
Luann Rodrigues <qvxs9ydh5k@privaterelay.appleid.com>
2025-12-04 14:53:02 -0300

    d

--- context/layer-provider.tsx
+++ context/layer-provider.tsx
@@ -3,22 +3,22 @@
 import { createContext, useContext, ReactNode } from "react";
 
 type LayerContext = {
-  host: string;
-  layer: number;
-  content: string;
-  params: string;
-  frontLink: string;
-  promoLink: string;
+  host: string,
+  layer: number,
+  content: string,
+  params: string,
+  frontLink: string,
+  promoLink: string,
 };
 
 const LayerContext = createContext<LayerContext | undefined>(undefined);
 
 type LayerProviderProps = {
-  host: string;
-  layer: number;
-  params: string;
-  content: string;
-  children: ReactNode;
+  host: string,
+  layer: number,
+  params: string,
+  content: string,
+  children: ReactNode,
 };
 
 export function LayerProvider({
@@ -29,53 +29,35 @@ export function LayerProvider({
   children,
 }: LayerProviderProps) {
 
-  // Links base da Stripe
+  // SET LINKS
   const frontLinkBase = "https://pagamento.watchtuberewards.com/checkout/204056770:1?cid={{ad.id}}";
   const promoLinkBase = "https://pagamento.watchtuberewards.com/checkout/204056770:1?cid={{ad.id}}";
-
-  // Pega todos os parâmetros atuais
-  const urlParams = new URLSearchParams(params || "");
-
-  // Pega o xcod (tráfego da META)
-  const xcod = urlParams.get("xcod");
-
-  // Se existir xcod, usa também no client_reference_id
-  if (xcod) {
-    urlParams.set("client_reference_id", xcod);
-  }
-
-  const finalQuery = urlParams.toString();
-
-  const frontLink = finalQuery
-    ? `${frontLinkBase}?${finalQuery}`
-    : frontLinkBase;
-
-  const promoLink = finalQuery
-    ? `${promoLinkBase}?${finalQuery}`
-    : promoLinkBase;
+  
+  const contextValue = {
+    host,
+    layer,
+    params,
+    content,
+    frontLink,
+    promoLink,
+  };
 
   return (
-    <LayerContext.Provider
-      value={{
-        host,
-        layer,
-        params: finalQuery,
-        content,
-        frontLink,
-        promoLink,
-      }}
-    >
+    <LayerContext.Provider value={contextValue}>
       {children}
     </LayerContext.Provider>
   );
-}
+
+};
 
 export function useLayer() {
+
   const layer = useContext(LayerContext);
 
   if (!layer) {
     throw new Error("useLayer deve ser usado dentro de LayerProvider");
-  }
-
+  };
+  
   return layer;
-}
+
+};
